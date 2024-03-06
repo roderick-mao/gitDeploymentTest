a4e8a2b53a7238c0de8e51ecca03bf82fd7035cd
v-zirunmao <v-zirunmao@microsoft.com>
2024-03-06 14:25:41 +0800

    init

---
+++ src/main/java/com/example/gitdeploymenttest/GitDeploymentTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitdeploymenttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDeploymentTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDeploymentTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitdeploymenttest/GitDeploymentTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitdeploymenttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDeploymentTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
